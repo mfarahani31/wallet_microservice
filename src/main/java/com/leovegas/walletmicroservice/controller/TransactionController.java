70a22d1af2840ece0d431044f74dd45697c91dd8
Mohammad Farahani <farahani.dev@gmail.com>
2022-10-06 17:40:26 +0330

    add some logger to controller layer

--- src/main/java/com/leovegas/walletmicroservice/controller/TransactionController.java
+++ src/main/java/com/leovegas/walletmicroservice/controller/TransactionController.java
@@ -4,6 +4,8 @@ import com.leovegas.walletmicroservice.dto.TransactionRequestDTO;
 import com.leovegas.walletmicroservice.dto.TransactionResponseDTO;
 import com.leovegas.walletmicroservice.mapper.TransactionMapper;
 import com.leovegas.walletmicroservice.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class TransactionController {
+    private static final Logger logger =
+            LoggerFactory.getLogger(TransactionController.class);
+    private static final String NEW_TRANSACTION_LOG = "New transaction was created :{}";
+
     private final TransactionService transactionService;
 
     @Autowired
@@ -24,12 +30,18 @@ public class TransactionController {
 
     @PostMapping("/credit/{userId}")
     public ResponseEntity<TransactionResponseDTO> creditByUserId(@PathVariable Long userId, @Valid @RequestBody TransactionRequestDTO transactionRequestDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.transactionService.creditByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO)));
+        TransactionResponseDTO responseDTO = this.transactionService.creditByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO));
+        logger.info(NEW_TRANSACTION_LOG, responseDTO.toString());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @PostMapping("/debit/{userId}")
     public ResponseEntity<TransactionResponseDTO> debitByUserId(@PathVariable Long userId, @Valid @RequestBody TransactionRequestDTO transactionRequestDTO) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.transactionService.debitByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO)));
+        TransactionResponseDTO responseDTO = this.transactionService.debitByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO));
+        logger.info(NEW_TRANSACTION_LOG, responseDTO.toString());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @GetMapping("/byUserId/{userId}")
