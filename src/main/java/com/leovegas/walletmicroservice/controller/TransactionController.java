85ce9d6cf306ec0bd7f1c9debca0e27689494c25
Mohammad Farahani <farahani.dev@gmail.com>
2022-10-06 17:53:53 +0330

    add some info for APIs by swagger annotations

--- src/main/java/com/leovegas/walletmicroservice/controller/TransactionController.java
+++ src/main/java/com/leovegas/walletmicroservice/controller/TransactionController.java
@@ -4,6 +4,10 @@ import com.leovegas.walletmicroservice.dto.TransactionRequestDTO;
 import com.leovegas.walletmicroservice.dto.TransactionResponseDTO;
 import com.leovegas.walletmicroservice.mapper.TransactionMapper;
 import com.leovegas.walletmicroservice.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
@@ -28,7 +34,9 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    @PostMapping("/credit/{userId}")
+    @Operation(summary = "Crate a new Credit transaction")
+    @ApiResponse(responseCode = "201", description = "Transaction credit is created", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TransactionResponseDTO.class))})
+    @PostMapping(value = "/credit/{userId}", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<TransactionResponseDTO> creditByUserId(@PathVariable Long userId, @Valid @RequestBody TransactionRequestDTO transactionRequestDTO) {
         TransactionResponseDTO responseDTO = this.transactionService.creditByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO));
         logger.info(NEW_TRANSACTION_LOG, responseDTO.toString());
@@ -36,7 +44,9 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
-    @PostMapping("/debit/{userId}")
+    @Operation(summary = "Crate a new Debit transaction")
+    @ApiResponse(responseCode = "201", description = "Transaction debit is created", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TransactionResponseDTO.class))})
+    @PostMapping(value = "/debit/{userId}", consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<TransactionResponseDTO> debitByUserId(@PathVariable Long userId, @Valid @RequestBody TransactionRequestDTO transactionRequestDTO) {
         TransactionResponseDTO responseDTO = this.transactionService.debitByUserId(userId, TransactionMapper.INSTANCE.toTransaction(transactionRequestDTO));
         logger.info(NEW_TRANSACTION_LOG, responseDTO.toString());
@@ -44,7 +54,9 @@ public class TransactionController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
-    @GetMapping("/byUserId/{userId}")
+    @Operation(summary = "Get list of transactions by userId for a specific user")
+    @ApiResponse(responseCode = "200", description = "Found the transactions", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = TransactionResponseDTO.class))})
+    @GetMapping(value = "/byUserId/{userId}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<List<TransactionResponseDTO>> getAllByUserId(@PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(this.transactionService.getAllByUserId(userId));
     }
--- src/main/java/com/leovegas/walletmicroservice/controller/UserController.java
+++ src/main/java/com/leovegas/walletmicroservice/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.leovegas.walletmicroservice.controller;
 
 import com.leovegas.walletmicroservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -19,6 +25,8 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get current balance of a user by userId")
+    @ApiResponse(responseCode = "200", description = "Found the current balance", content = {@Content(mediaType = APPLICATION_JSON_VALUE, schema = @Schema(implementation = Long.class))})
     @GetMapping("/currentBalance/{userId}")
     public ResponseEntity<Long> getCurrentBalance(@PathVariable Long userId) {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.getCurrentBalance(userId));
